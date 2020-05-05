0c1c7b179c2f4b3918301bd4aa61a218f2e762c8
Lincoln Rychecky <54992499+LincolnRychecky@users.noreply.github.com>
2020-05-05 08:59:34 -0600

    Update twitterbot.py

--- twitterbot.py
+++ twitterbot.py
@@ -1,18 +1,23 @@
 import tweepy
 import requests
 import json
+from datetime import datetime
+#DateTime==4.3
+
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler("i1Uhg4JlVD9ce3Nq6AVrHHcDx", "chD57OvgbzulKOaUsEFosyFJj0llKIqAJf73oi0XGAbCIUDyHm")
 auth.set_access_token("1250474930178670593-Nm4zBI8sIB4S14mTyQ6Cbyq6vvie0f", "szTNXDOpipi2za5hcETflqNiJDn0cFmQxVN9eeD673U8k")
-
 # Create API object
 api = tweepy.API(auth)
 
+#Retrieve current date
+curDate = str(datetime.date(datetime.now()))
+#Make request from nasa json for data on current date
+url = "https://api.nasa.gov/neo/rest/v1/feed?start_date="+curDate+"&end_date="+curDate+"&api_key=nTiezUKlDZQpC3TNQ1NUxGuLqQRDhfQYOdd3HBFa"
+reqJson = requests.get(url)
+#Parse json into a dictionary
+parsed_json = (json.loads(reqJson.text))
+numberNeos = str(parsed_json['element_count'])
+
 # Create a tweet
-#api.update_status("Hello Tweepy this is my second call to this asdfa asdjfaksjdhfkajhfk aksjdb ugcvugcvujgcjg")
-# Make request from nasa json
-url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2020-04-14&end_date=2020-04-15&api_key=nTiezUKlDZQpC3TNQ1NUxGuLqQRDhfQYOdd3HBFa"
-asteroidDict = requests.get(url)
-json_data = '{"a": 1, "b": 2, "c": 3, "d": 4, "e": 5}'
-pasrsed_json = (json.loads(asteroidDict.text))
-print()
+api.update_status(numberNeos + " NEOs passed earth today. That is a slow day in near earth space!")
